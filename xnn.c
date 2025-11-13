39ea48480bd03d942606daf5039e1a4c71a204ea
Peter Jepson <peterjepson123@googlemail.com>
2025-11-13 21:37:32 +0000

    Matrix Struct

--- xnn.c
+++ xnn.c
@@ -1,7 +1,17 @@
+#define XNN_IMPLEMENTATION
 #include "xnn.h"
+
 
 int main()
 {
+	Matrix *m = matrix_alloc(20,20);
+	if(!m) {
+		fprintf(stderr, "ERROR: Matrix allocation Failed");
+		return -1;
+	}
+
+	matrix_fill(m, 0.0f);
+	matrix_print(m);
 
 
 	return 0;
--- xnn.h
+++ xnn.h
@@ -4,9 +4,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+	size_t rows;
+	size_t cols;
+	float *data;
+
+} Matrix;
+
 #endif
 
 #ifdef XNN_IMPLEMENTATION
+
+Matrix *matrix_alloc(size_t rows, size_t cols);
+void matrix_fill(Matrix *m, float val);
+void matrix_print(Matrix *m);
+
+Matrix *matrix_alloc(size_t rows, size_t cols)
+{
+	Matrix *m;
+	m = malloc(sizeof(Matrix));
+	if(!m) return NULL;
+	m->rows = rows;
+	m->cols = cols;
+	m->data = malloc(sizeof(float) * rows * cols);
+	if(!m->data) return NULL;
+
+	return m;
+}
+
+void matrix_fill(Matrix *m, float val)
+{
+	size_t n = m->rows * m->cols;
+	for(size_t i = 0; i < n; i++) {	
+		m->data[i] = val;
+	}
+
+	return;
+}
+
+void matrix_print(Matrix *m)
+{
+	for(size_t i = 0; i < m->rows; i++) {
+		for(size_t j = 0; j < m->cols; j++) {
+			printf("%2.2f ", m->data[i * m->cols + j]);
+		}
+		printf("\n");
+	}
+
+	return;
+}
+
 
 
 
